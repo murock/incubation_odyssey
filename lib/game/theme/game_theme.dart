3ff3b17b030852a00d917c61764e69c7bea0b5f9
BrunoJay <brunojay001@gmail.com>
2023-12-10 15:27:13 +0300

    feat: added temperature text

---
+++ lib/game/theme/game_theme.dart
@@ -0,0 +1,11 @@
+import 'package:flame/components.dart';
+import 'package:flutter/material.dart';
+
+class GameTheme {
+  static TextPaint regular = TextPaint(
+      style: const TextStyle(
+    fontSize: 36, // Adjust the font size as needed
+    color: Colors.white,
+    fontFamily: 'SinglyLinked',
+  ));
+}
