a13c89243948c08c040ccbe704674cabe43ad887
murock <alfiebpage@gmail.com>
2023-12-10 13:47:17 +0000

    Randomise spawner

--- lib/game/variables.dart
+++ lib/game/variables.dart
@@ -13,7 +13,7 @@ class Variables {
   static double powerUpHeight = 300;
 
   /// spawn rate for power up in seconds
-  static double initialpowerUpSpawnRate = 4;
+  static double maxPowerUpSpawnTime = 4;
 
   static double playerSpeed = 120;
 
